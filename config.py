959b59bf7c231c03fa07f2185cb0417564489afd
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-11 22:26:25 +0530

    Update API credentials and owner ID in config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 
-API_ID = os.environ.get("API_ID", "25149550")
+API_ID = os.environ.get("API_ID", "22447622")
 
-API_HASH = os.environ.get("API_HASH", "67b06e7a112cb9b9a5365c5f4fce2495")
+API_HASH = os.environ.get("API_HASH", "543b62d58d3e723e766ba57a984ab65d")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8387440827:AAFuE8cD6GS7ShNwovdRCGLc49x59gD1Ufo")
 
 PASS_DB = int(os.environ.get("PASS_DB", "720"))
 
-#OWNER = int(os.environ.get("OWNER", "7899819345"))
+#OWNER = int(os.environ.get("OWNER", "777756062"))
 
-#LOG = ,
+#LOG = -1003523745178,
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[7899819345]
-    #for x in (os.environ.get("ADMINS", "7899819345").split()):
+    #ADMINS=[777756062]
+    #for x in (os.environ.get("ADMINS", "777756062").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
